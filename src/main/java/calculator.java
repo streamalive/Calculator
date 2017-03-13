6f422f0a5686d9278abd23247d1c80269c405ecf
hostMPKFA <userhost1@gmail.vom>
2017-03-13 19:32:51 +0300

    Initial commit

---
+++ src/main/java/calculator.java
@@ -0,0 +1,10 @@
+
+public class calculator {
+    public int getSumm (int firstX, int secondX){
+        return firstX + secondX;
+    }
+
+    public  int getMinus (int firstX, int secondX) {
+        return firstX - secondX;
+    }
+}
